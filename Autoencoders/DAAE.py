691a810dafe9e7bf07dc5386f3fc9169f4cbe1cf
Marie-Judith Saint Martin <asm95@orange.fr>
2018-06-07 17:43:15 +0200

    modification autoencoder

---
+++ Autoencoders/DAAE.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils as utils
+from torch.autograd import Variable
+
+class Autoencoder(nn.Module):
+    def __init__(self, nz=2):
+        super(Autoencoder, self).__ini__()
+        self.nz=nz
+        self.encoder=nn.Sequential(
+        nn.Linear(28*28,128),
+        nn.Tanh(),
+        nn.Linear(128,64),
+        nn.Tanh(),
+        nn.Linear(64,12),
+        nn.Tanh()
+        nn.Linear(12,nz),
+        )
+        self.decoder=nn.Sequential(
+        nn.Linear(nz,12),
+        nn.Tanh(),
+        nn.Linear(12,64),
+        nn.Tanh(),
+        nn.Linear(64,128),
+        nn.Tanh(),
+        nn.Linear(128,28*28),
+        nn.Sigmoid()
+        )
+        
+    def forward(self,x):
+        encoded=self.encoder(x)
+        decoded=self.decoder(encoded)
+        return encoded, decoded
+        
+    def lossfunc(self, encoded,original)
+        loss=nn.MSELoss()
+        return loss(decoded, original)
