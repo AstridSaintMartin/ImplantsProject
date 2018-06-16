26887b213eca3addaf2a3b11005708bc955379e1
Marie-Judith Saint Martin <asm95@orange.fr>
2018-06-16 12:06:31 +0100

    code used to displayt the dicom images

--- displayImage.py
+++ displayImage.py
@@ -6,9 +6,34 @@ import os
 import csv
 import pydicom
 
-def plotImage(root,num_image):
-    Pathdicom=os.path.join(root,"Patients/10915212/001")
-    d=pydicom.read_file(Pathdicom)
+DICT={}
+dict={}
+
+def plotImage(root):
+    Pathdicom=os.path.join(root,"Patients2")
+    for (dirpath, dirnames, filenames) in os.walk(Pathdicom):
+        for filename in filenames:
+            #
+            #create a directory for each patient inside the folder images
+            new_dir =os.path.join(root,"Images",dirpath[-7:])
+            if not os.path.isdir(new_dir):
+                os.makedirs(new_dir)
+            #transform dicom image into numpy array
+            d=pydicom.read_file(os.path.join(dirpath,filename))
+            d_pix_ar = d.pixel_array
+            array = np.array(d_pix_ar)
+            global dict
+            dict[filename]=array
+            figure=plt.figure()
+            plt.imshow(array, interpolation='nearest', cmap="gray")
+            figure.savefig(os.path.join(new_dir,filename)+".png")
+            plt.close()
+        global DICT
+        DICT[dirpath[-7:]]=dict
     
-    
-plotImage("/Users/astrid/Documents/ImplantsProject",1)
+
+if __name__=='__main__':
+    plotImage("/Users/astrid/Documents/ImplantsProject")
+    print len(DICT)
+    print len(dict)
+
