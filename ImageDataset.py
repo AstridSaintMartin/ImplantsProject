b79fbd706f4830744b103983f33c734d66d488b4
Marie-Judith Saint Martin <asm95@orange.fr>
2018-07-05 21:36:08 +0100

    changing the channel layout to use toTensor method

--- ImageDataset.py
+++ ImageDataset.py
@@ -36,20 +36,17 @@ class KneeDataset(Dataset):
         array_of_patient=self.dict[row_csv["PatientsID"]]
         array=array_of_patient[row_csv["StackNumber"]]
         array=array.astype(np.float32)
-        array= np.expand_dims(array, axis=0)
-        array=torch.from_numpy(array)
-        #print "non normalis",array
+        array= np.expand_dims(array, axis=2)
+        #array=torch.from_numpy(array)
         if self.transform is not None:
             array=self.transform(array)
-            #print "normalise",array
-        print array.size()
         return array, label
 
 
 normalize = transforms.Normalize(mean=[20], std=[30])
-Trans=transforms.Compose([normalize])
+Trans=transforms.Compose([transforms.ToTensor(),normalize])
 dset=KneeDataset(csv_file="DATA/DatasetKnee.csv",dict_file="DATA/dictImages64x64.pkl", transform=Trans)
-dataloader = DataLoader(dset, batch_size=1,shuffle=True)
+dataloader = DataLoader(dset, batch_size=4,shuffle=True)
 img, label=next(iter(dataloader))
 print img
 print torch.mean(img)
